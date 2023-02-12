b17353abe5867b6abe16639d4cf84ca4f3beafd1
Vlad <vladstoyanoff@gmail.com>
2023-02-12 18:38:19 +0000

    Readme

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,33 +1,33 @@
-import { useEffect, useState } from "react";
-import styles from "../styles/errors.module.scss";
+// import { useEffect, useState } from "react";
+// import styles from "../styles/errors.module.scss";
 
-function ErrorMessage({ code, action, setError }) {
-  const [shown, setShown] = useState(false);
+// function ErrorMessage({ code, action, setError }) {
+//   const [shown, setShown] = useState(false);
 
-  useEffect(() => {
-    if (code === null) return;
-    setShown(true);
-    const timer = setTimeout(hide, 3000);
+//   useEffect(() => {
+//     if (code === null) return;
+//     setShown(true);
+//     const timer = setTimeout(hide, 3000);
 
-    return () => clearTimeout(timer);
-  }, [code]);
+//     return () => clearTimeout(timer);
+//   }, [code]);
 
-  function hide() {
-    setError(null);
-    setShown(false);
-  }
+//   function hide() {
+//     setError(null);
+//     setShown(false);
+//   }
 
-  let message = "";
+//   let message = "";
 
-  if (code === 403 || code === 401) {
-    message = `Please login to ${action}`;
-  }
-  return (
-    <div className={shown ? styles.wrapper : styles.hidden}>
-      <p>{`${message}`}</p>
-      <button onClick={hide}>CLOSE</button>
-    </div>
-  );
-}
+//   if (code === 403 || code === 401) {
+//     message = `Please login to ${action}`;
+//   }
+//   return (
+//     <div className={shown ? styles.wrapper : styles.hidden}>
+//       <p>{`${message}`}</p>
+//       <button onClick={hide}>CLOSE</button>
+//     </div>
+//   );
+// }
 
-export default ErrorMessage;
+// export default ErrorMessage;
